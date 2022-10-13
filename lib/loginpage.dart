91aa9401a734b4a9f0d7c73f43dc6ce79c4a4acd
umerkarippol <umerkarippol@gmail.com>
2022-10-13 13:53:24 +0530

    loginpage

---
+++ lib/loginpage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class loginpage extends StatefulWidget {
+  const loginpage({Key? key}) : super(key: key);
+
+  @override
+  State<loginpage> createState() => _loginpageState();
+}
+
+class _loginpageState extends State<loginpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+    
+  }
+}
